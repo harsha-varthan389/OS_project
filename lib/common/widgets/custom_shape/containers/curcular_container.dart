496d11ca0932c13b9cf4589ba12f9f054c507b68
SANTO PAUL M <santo@SANTOs-MacBook-Air.local>
2024-11-10 14:31:00 +0530

    added vid 15

--- lib/common/widgets/custom_shape/containers/curcular_container.dart
+++ lib/common/widgets/custom_shape/containers/curcular_container.dart
@@ -3,11 +3,19 @@ import 'package:flutter/material.dart';
 
 class TCircularContainer extends StatelessWidget {
   const TCircularContainer({
-    super.key, this.width = 400, this.height = 400,  this.radius = 400,  this.padding = 0, this.child,  this.backgroundColor = TColors.white,
+    super.key,
+    this.width = 400,
+    this.height = 400,
+    this.radius = 400,
+    this.margin,
+    this.padding = 0,
+    this.child,
+    this.backgroundColor = TColors.white,
   });
   final double? width;
   final double? height;
   final double radius;
+  final EdgeInsets? margin;
   final double padding;
   final Widget? child;
   final Color backgroundColor;
@@ -15,8 +23,9 @@ class TCircularContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 400,
-      height: 400,
+      width: width,
+      height: height,
+      margin: margin,
       padding: const EdgeInsets.all(0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(400),
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,26 +1,26 @@
 //import
 
-
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_commerce/common/widgets/custom_shape/containers/curcular_container.dart';
 import 'package:e_commerce/common/widgets/custom_shape/containers/primary_header_container.dart';
 import 'package:e_commerce/common/widgets/custom_shape/containers/search_container.dart';
 import 'package:e_commerce/common/widgets/images/t_rounded_images.dart';
 import 'package:e_commerce/common/widgets/texts/section_heading.dart';
 import 'package:e_commerce/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:e_commerce/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:e_commerce/features/shop/screens/home/widgets/promo_slider.dart';
 import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:e_commerce/utils/constants/image_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return   Scaffold(
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -29,15 +29,16 @@ class HomeScreen extends StatelessWidget{
                 children: [
                   ThomeAppBar(),
                   SizedBox(height: TSizes.spaceBtwSections),
-
                   TSearchContainer(text: 'Search in store'),
                   SizedBox(height: TSizes.spaceBtwSections),
-
                   Padding(
                     padding: EdgeInsets.only(left: TSizes.defaultSpace),
                     child: Column(
                       children: [
-                        TSectionHeading(title: 'Popular Categories',showActionButton: false, textColor: Colors.white),
+                        TSectionHeading(
+                            title: 'Popular Categories',
+                            showActionButton: false,
+                            textColor: Colors.white),
                         SizedBox(height: TSizes.spaceBtwItems),
 
                         //categories
@@ -45,27 +46,18 @@ class HomeScreen extends StatelessWidget{
                       ],
                     ),
                   )
-
                 ],
               ),
             ),
 
             //Body
             Padding(
-              padding: const EdgeInsets.all(TSizes.defaultSpace),
-              child: CarouselSlider(
-                options: CarouselOptions(
-                  viewportFraction: 0.8
-                ),
-                items: [
-                  TRoundedImage(imageUrl: TImages.promoBanner3),
-                ],)
-            )
+                padding: const EdgeInsets.all(TSizes.defaultSpace),
+                child: TPromoSlider(banners: [TImages.promoBanner1,TImages.promoBanner2,TImages.promoBanner3],))
           ],
         ),
       ),
     );
   }
 }
-
 
